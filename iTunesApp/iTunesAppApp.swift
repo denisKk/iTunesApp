adb9446e24a679131791a3ddf5ae2b4ec195fde8
Denis Kulakov <kulakovdenis@mail.ru>
2022-09-20 21:40:43 +0300

    Initial Commit

---
+++ iTunesApp/iTunesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesAppApp.swift
+//  iTunesApp
+//
+//  Created by Dev on 20.09.22.
+//
+
+import SwiftUI
+
+@main
+struct iTunesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
